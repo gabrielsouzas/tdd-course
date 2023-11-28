63ab724f72526aadb09da1a5590dfb8884ce0f00
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-11-28 13:08:32 -0300

    Primeiro teste

---
+++ src/Pilha.java
@@ -0,0 +1,25 @@
+import java.util.function.BooleanSupplier;
+
+public class Pilha {
+	
+	private Object elemento;
+	private int quantidade;
+
+	public boolean estaVazia() {
+		return (elemento == null);
+	}
+
+	public int tamanho() {
+		return quantidade;
+	}
+
+	public void empilha(Object elemento) {
+		this.elemento = elemento;
+		quantidade++;
+	}
+
+	public Object topo() {
+		return elemento;
+	}
+
+}
